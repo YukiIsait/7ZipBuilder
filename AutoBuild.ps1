d1c5a5641fd54031dca464372bd27b8b741a6ae7
YukiIsait <yukiisait@outlook.jp>
2024-08-20 20:39:49 +0800

    Style: 添加注释

--- AutoBuild.ps1
+++ AutoBuild.ps1
@@ -1,9 +1,14 @@
-param(
+﻿param(
     [string] $buildVersion
 )
 
 $workDir = $PSScriptRoot
 
+# 下载源代码并替换资源文件
 & "$workDir\Prepare.ps1" $buildVersion
+
+# 自动查找Visual Studio工具链并进行编译
 & "$workDir\Build.ps1" $buildVersion
+
+# 导出编译好的文件并制作安装包
 & "$workDir\Pack.ps1" $buildVersion
