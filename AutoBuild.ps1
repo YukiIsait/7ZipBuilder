756a9c9569561fdce93b705528d6e829f0a3b65a
YukiIsait <yukiisait@outlook.jp>
2024-12-03 12:03:56 +0800

    Fix: 修复流程失败会继续执行的问题

--- AutoBuild.ps1
+++ AutoBuild.ps1
@@ -6,10 +6,10 @@
 $workDir = $PSScriptRoot
 
 # 下载源代码并替换资源文件
-& "$workDir\Prepare.ps1" $buildVersion
+& "$workDir\Prepare.ps1" $buildVersion -ErrorAction Stop
 
 # 自动查找Visual Studio工具链并进行编译
-& "$workDir\Build.ps1" $buildVersion
+& "$workDir\Build.ps1" $buildVersion -ErrorAction Stop
 
 # 导出编译好的文件并制作安装包
-& "$workDir\Pack.ps1" $buildVersion
+& "$workDir\Pack.ps1" $buildVersion -ErrorAction Stop
